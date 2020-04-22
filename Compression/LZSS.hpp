2b35c7957c5d5f51a8bccaa9e2fbb3629b518ac0
Robert Russell <peeceseven@gmail.com>
2020-04-22 14:04:32 -0400

    WIP on compress. copied the gpl notice from deling to put with the compress code
    I'm changing his code very little.

--- Compression/LZSS.hpp
+++ Compression/LZSS.hpp
@@ -97,6 +97,33 @@ public:
   }
   // todo add compression method
 
+/****************************************************************************
+ ** Deling Final Fantasy VIII Field Editor
+ ** Copyright (C) 2009-2012 Arzel Jérôme <myst6re@gmail.com>
+ **
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ **
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+/***************************************************************
+	4/6/1989 Haruhiko Okumura
+	Use, distribute, and modify this program freely.
+	Please send me your improved versions.
+		PC-VAN		SCIENCE
+		NIFTY-Serve	PAF01022
+		CompuServe	74050,1022
+*************************************************************
+ * Porting to STL - sebanisu.
+ * */
 
   [[nodiscard]] [[maybe_unused]] static auto Compress(const std::vector<unsigned char>& src)
   {
