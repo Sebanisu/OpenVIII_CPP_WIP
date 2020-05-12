a78ecd881d445cf5d0415cb6a41c0ad4d596e386
Robert Russell <peeceseven@gmail.com>
2020-05-11 22:06:03 -0400

    cleaned up GetAllEntriesData

--- Libs/Archive/FL.h
+++ Libs/Archive/FL.h
@@ -28,61 +28,57 @@ private:
     }
   }
 
-  template<class Container, typename idType>
-  static void splitAndInsert(const std::string &str, Container &cont, idType &id, char delimiter)
-  {
-    std::stringstream ss(str);
-    std::string token;
-    while (std::getline(ss, token, delimiter)) {
-      CleanString(token);
-      cont.insert(std::make_pair(id++, std::move(token)));
-    }
-  }
 
 public:
   constexpr const static auto Ext = std::string_view(".FL");
 
-  [[nodiscard]] static auto
-    GetAllEntriesData(const std::filesystem::path &path, const std::string &data, const size_t &offset)
+  [[nodiscard]] static auto GetAllEntriesData(const std::filesystem::path &path,
+    const std::string &data,
+    const size_t &offset,
+    const size_t count = 0)
   {
-    struct comp
-    {
-      bool operator()(const std::pair<unsigned int, std::string> &left,
-        const std::pair<unsigned int, std::string> &right) const
-      {
-        if (left.second.length() < right.second.length()) { return true; }
-        if (left.second.length() > right.second.length()) { return false; }
-        return left.second < right.second;
-        // string < string caused assert failure.
-      }
-    };
 
-    auto set = std::set<std::pair<unsigned int, std::string>, comp>();
-    std::ifstream fp;
+    std::ifstream fp{};
+    auto vector = std::vector<std::pair<unsigned int, std::string>>();
+    if (!data.empty() || (fp = std::ifstream(path, std::ios::in)).is_open()) {
+      if (count > 0) vector.reserve(count);
 
-    unsigned int id = 0;
-    std::string innerPath;
-    auto loop = [&id, &offset, &fp, &innerPath, &set]() mutable {
-      fp.seekg(static_cast<long>(offset), std::ios::beg);
-      while (std::getline(fp, innerPath)) {
-        CleanString(innerPath);
-        set.insert(std::make_pair(id++, std::move(innerPath)));
+      const auto loop = [&vector](auto &cont) {
+        unsigned int id = 0;
+
+        std::string innerPath;
+        while (std::getline(cont, innerPath, '\n')) {
+          CleanString(innerPath);
+          vector.emplace_back(std::make_pair(id++, std::move(innerPath)));
+        }
+      };
+
+      if (fp.is_open()) {
+        fp.seekg(static_cast<long>(offset));
+        loop(fp);
+        if (!fp.is_open()) { fp.close(); }
+      } else {
+        if (data[0] == '\0') {
+          std::cout << "\033[1;31mFL Data is null!\033[0m\n";
+          // exit(EXIT_FAILURE);
+          return vector;
+        }
+        auto ss = std::stringstream(data);
+        loop(ss);
       }
-      if (!fp.is_open()) { fp.close(); }
-      return set;
-    };
-    if (!data.empty()) {
-      if (data[0] == '\0') {
-        std::cout << "\033[1;31mFL Data is null!\033[0m\n";
-        return set;
-      }
-      splitAndInsert(data, set, id, '\n');
-    } else {
-      fp = std::ifstream(path, std::ios::in);
-      if (!fp.is_open()) { return set; }
-      loop();
+
+      std::sort(vector.begin(),
+        vector.end(),
+        [](const auto &left, const auto &right) {
+          if (left.second.length() < right.second.length()) { return true; }
+          if (left.second.length() > right.second.length()) { return false; }
+          return left.second < right.second;
+        });
+      // no real way to know the number of lines here. FI can know the number because the count is the filesize /12.
+      // pass the count as an argument to reserve the correct number of elements.
+      vector.shrink_to_fit();
     }
-    return set;
+    return vector;
   }
   [[maybe_unused]] [[nodiscard]] static auto GetAllEntries(const std::filesystem::path &path, const size_t &offset)
   {
@@ -94,7 +90,7 @@ public:
   {
     std::ifstream fp = std::ifstream(path, std::ios::in);
     unsigned int i = 0;
-    fp.seekg(static_cast<long>(offset), std::ios::beg);
+    fp.seekg(static_cast<long>(offset));
     std::string innerPath;
     while (std::getline(fp, innerPath)) {
       if (Tools::iFind(innerPath, needle)) {
