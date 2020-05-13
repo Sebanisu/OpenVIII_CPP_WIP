4bd6dec0a1f916e09dbc7a05bcc3ec1ef287351d
Robert Russell <peeceseven@gmail.com>
2020-05-12 22:05:49 -0400

    refactor FIFLFS by grouping multiple varibles into a sub struct. so I can return all the varibles at once now. first draft of string search works.

--- Libs/Archive/FIFLFS.h
+++ Libs/Archive/FIFLFS.h
@@ -18,87 +18,91 @@ namespace OpenVIII::Archive {
 struct FIFLFS
 {
 private:
-  std::filesystem::path fi_{ "" };
-  std::filesystem::path fs_{ "" };
-  std::filesystem::path fl_{ "" };
-  size_t fiOffset_{ 0U };
-  size_t fsOffset_{ 0U };
-  size_t flOffset_{ 0U };
-  size_t count_{ 0U };
-  std::vector<char> fiData_{};
-  std::vector<char> fsData_{};
-  std::basic_string<char> flData_{};// this is char because the file contains strings.
-  std::string fiBase_{};
-  std::string fsBase_{};
-  std::string flBase_{};
+  template<typename T> struct ds_
+  {
+    std::filesystem::path path{};
+    size_t offset{};
+    T data{};
+    std::string base{};
+
+    // Assigns basename and returns it.
+    std::string GetBaseName() { return base = FIFLFS::GetBaseName(path); }
+    operator bool() const { return (!path.empty() && std::filesystem::exists(path)) || !std::empty(data); }
+  };
+  ds_<std::vector<char>> fi_{};
+  ds_<std::vector<char>> fs_{};
+  ds_<std::basic_string<char>> fl_{};// this is char because the file contains strings.
+  size_t count_{};
 
 public:
-  [[maybe_unused]] [[nodiscard]] auto FI() const { return fi_; }
+  //  [[maybe_unused]] [[nodiscard]] auto FI() const noexcept { return fi_; }
+  //
+  //  [[maybe_unused]] [[nodiscard]] auto FS() const noexcept { return fs_; }
+  //
+  //  [[maybe_unused]] [[nodiscard]] auto FL() const noexcept { return fl_; }
 
-  [[maybe_unused]] [[nodiscard]] auto FS() const { return fs_; }
+  [[maybe_unused]] [[nodiscard]] const auto &FI() const noexcept { return fi_; }
 
-  [[maybe_unused]] [[nodiscard]] auto FL() const { return fl_; }
+  [[maybe_unused]] [[nodiscard]] const auto &FS() const noexcept { return fs_; }
 
-  [[maybe_unused]] [[nodiscard]] auto FIData() const { return fiData_; }
+  [[maybe_unused]] [[nodiscard]] const auto &FL() const noexcept { return fl_; }
 
-  [[maybe_unused]] [[nodiscard]] auto FSData() const { return fsData_; }
-
-  [[maybe_unused]] [[nodiscard]] auto FLData() const { return flData_; }
-  [[maybe_unused]] void FI(const std::filesystem::path &fi, const size_t &offset = 0U, size_t size = 0U)
+  [[maybe_unused]] void FI(const std::filesystem::path &path, const size_t &offset = 0U, size_t size = 0U)
   {
-    fi_ = fi;
-    fiOffset_ = offset;
-    if (size == 0U) {
-      if (std::filesystem::exists(fi_)) { size = std::filesystem::file_size(fi_); }
-    }
+    fi_.path = path;
+    fi_.offset = offset;
+    if (size == 0U && std::filesystem::exists(fi_.path)) { size = std::filesystem::file_size(fi_.path); }
     count_ = FI::GetCount(size);
   }
-  void FI(const std::filesystem::path &fi, const std::vector<char> &vector, const size_t &offset = 0U)
+  void FI(const std::filesystem::path &path, const std::vector<char> &vector, const size_t &offset = 0U)
   {
-    FI(fi, offset, vector.size());
-    if (!vector.empty()) { fiData_ = vector; }
+    FI(path, offset, vector.size());
+    if (!vector.empty()) { fi_.data = vector; }// todo std::move into this.
   }
 
-  void FS(const std::filesystem::path &fs, const size_t &offset = 0U)
+  void FS(const std::filesystem::path &path, const size_t &offset = 0U)
   {
-    fs_ = fs;
-    fsOffset_ = offset;
+    fs_.path = path;
+    fs_.offset = offset;
   }
-  void FS(const std::filesystem::path &fs, const std::vector<char> &vector, const size_t &offset = 0U)
+  void FS(const std::filesystem::path &path, const std::vector<char> &vector, const size_t &offset = 0U)
   {
-    FS(fs, offset);
-    if (!vector.empty()) { fsData_ = vector; }
-  }
-
-  void FL(const std::filesystem::path &fl, const size_t &offset = 0U)
-  {
-    fl_ = fl;
-    flOffset_ = offset;
+    FS(path, offset);
+    if (!vector.empty()) { fs_.data = vector; }// todo std::move into this.
   }
 
-  void FL(const std::filesystem::path &fl, const std::vector<char> &vector, const size_t &offset = 0U)
+  void FL(const std::filesystem::path &path, const size_t &offset = 0U)
   {
-    FL(fl, offset);
+    fl_.path = path;
+    fl_.offset = offset;
+  }
+
+  void FL(const std::filesystem::path &path, const std::vector<char> &vector, const size_t &offset = 0U)
+  {
+    // todo change vector to std::string
+    FL(path, offset);
     if (!vector.empty()) {
-      flData_ = std::basic_string<char>(vector.begin(), vector.end());
+      fl_.data =
+        std::basic_string<char>(vector.begin(), vector.end());// todo std::move after getting string instead of vector.
       // check for garbage
       auto index =
-        flData_.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890_\n\r\\:/.-");
+        fl_.data.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890_\n\r\\:/.-");
       if (index != std::string::npos) {
-        std::cerr << "\nError: Invalid character (" << static_cast<int>(flData_[index])
-                  << ") found in FL data! = " << fl << '\n';
+        std::cerr << "\nError: Invalid character (" << static_cast<int>(fl_.data.at(index))
+                  << ") found in FL data! = " << fl_.path << '\n';
         exit(EXIT_FAILURE);
       }
     }
   }
 
-  [[maybe_unused]] void FL(const std::filesystem::path &fl, const std::string_view &vector, const size_t &offset = 0U)
+  [[maybe_unused]] void
+    FL(const std::filesystem::path &path, const std::string_view &stringView, const size_t &offset = 0U)
   {
-    FL(fl, offset);
-    if (!vector.empty()) { flData_ = vector; }
+    FL(path, offset);
+    if (!stringView.empty()) { fl_.data = stringView; }// todo std::move after getting string instead of stringView.
   }
 
-  [[nodiscard]] bool AllSet() const { return (fi_ != "") && (fs_ != "") && (fl_ != ""); }
+  [[nodiscard]] bool AllSet() const { return fi_ && fs_ && fl_; }
 
   FIFLFS() = default;
 
@@ -109,27 +113,30 @@ public:
     const size_t &fsOffset = 0U,
     const size_t &flOffset = 0U)
   {
-    fi_ = fi;
-    fs_ = fs;
-    fl_ = fl;
-    fiOffset_ = fiOffset;
-    flOffset_ = flOffset;
-    fsOffset_ = fsOffset;
+    fi_.path = fi;
+    fs_.path = fs;
+    fl_.path = fl;
+    fi_.offset = fiOffset;
+    fl_.offset = flOffset;
+    fs_.offset = fsOffset;
+    // todo should i have this constructor? is it even used? the reason i think not setting all possible values and why
+    // would we want it to...
   }
 
   [[nodiscard]] friend std::ostream &operator<<(std::ostream &os, const FIFLFS &data)
   {
-    os << '{' << data.GetBaseName() << ", " << '{' << data.fi_ << ", " << data.fl_ << ", " << data.fs_ << "}}";
+    os << '{' << data.GetBaseName() << ", " << '{' << data.fi_.path << ", " << data.fl_.path << ", " << data.fs_.path
+       << "}}";
     return os;
   }
 
   [[nodiscard]] auto GetEntry(const unsigned int &id) const
   {
     if (id < count_ || count_ == 0) {
-      if (!fiData_.empty()) {
-        return FI::GetEntry(fiData_, id, fiOffset_);
+      if (!fi_.data.empty()) {
+        return FI::GetEntry(fi_.data, id, fi_.offset);
       } else {
-        return FI::GetEntry(fi_, id, fiOffset_);
+        return FI::GetEntry(fi_.path, id, fi_.offset);
       }
     }
     return OpenVIII::Archive::FI();
@@ -137,7 +144,7 @@ public:
 
   [[maybe_unused]] [[nodiscard]] auto GetEntry(const std::string_view &needle) const
   {
-    auto found = FL::GetEntry(fl_, needle, fiOffset_);
+    auto found = FL::GetEntry(fl_.path, needle, fi_.offset);
     const auto &[id, path] = found;
     return GetEntry(id);
   }
@@ -161,8 +168,8 @@ public:
   }
   void Test() const
   {
-    std::cout << "Getting Filenames from : " << fl_ << std::endl;
-    const auto entries = Archive::FL::GetAllEntriesData(fl_, flData_, flOffset_,count_);
+    std::cout << "Getting Filenames from : " << fl_.path << std::endl;
+    const auto entries = Archive::FL::GetAllEntriesData(fl_.path, fl_.data, fl_.offset, count_);
     auto archive = FIFLFS();
     for (const auto &item : entries) {
       const auto &[id, strPath] = item;
@@ -170,10 +177,10 @@ public:
 
       std::vector<char> buffer{};
       if (fi.UncompressedSize() > 0) {
-        if (!fsData_.empty()) {
-          buffer = FS::GetEntry(fsData_, fi, fsOffset_);
+        if (!fs_.data.empty()) {
+          buffer = FS::GetEntry(fs_.data, fi, fs_.offset);
         } else {
-          buffer = FS::GetEntry(fs_, fi, fsOffset_);
+          buffer = FS::GetEntry(fs_.path, fi, fs_.offset);
         }
       }
 
@@ -213,17 +220,17 @@ public:
     switch (CheckExtension(fileEntry.path())) {
     case 1:
       FL(fileEntry, vector, 0U);
-      flBase_ = FLBaseName();
+      fl_.GetBaseName();
       compareBaseNames();
       return AllSet() ? 2 : 1;
     case 2:
       FS(fileEntry, vector, 0U);
-      fsBase_ = FSBaseName();
+      fs_.GetBaseName();
       compareBaseNames();
       return AllSet() ? 2 : 1;
     case 3:
       FI(fileEntry, vector, 0U);
-      fiBase_ = FIBaseName();
+      fi_.GetBaseName();
       compareBaseNames();
       return AllSet() ? 2 : 1;
     default:
@@ -233,36 +240,24 @@ public:
   }
   void compareBaseNames()
   {
-    if ((flBase_ == fsBase_ && fiBase_ == fsBase_) || std::empty(flBase_) || std::empty(fiBase_)
-        || std::empty(fsBase_)) {
+    if ((fl_.base == fs_.base && fi_.base == fs_.base) || std::empty(fl_.base) || std::empty(fi_.base)
+        || std::empty(fs_.base)) {
       return;
     }
-    if (flBase_ != fsBase_ || fiBase_ != fsBase_) {
-      if (flBase_ == fiBase_) {
+    if (fl_.base != fs_.base || fi_.base != fs_.base) {
+      if (fl_.base == fi_.base) {
         std::cerr << "Removing FS Data: " << fs_ << "\n";
-        fsBase_ = std::string{};
-        fsData_ = std::vector<char>{};
-        fsOffset_ = 0;
-        fs_ = std::filesystem::path{};
-
+        fs_ = {};
         exit(EXIT_FAILURE);
       }
-    } else if (fiBase_ != flBase_) {
-      if (flBase_ == fsBase_) {
+    } else if (fi_.base != fl_.base) {
+      if (fl_.base == fs_.base) {
         std::cerr << "Removing FL Data: " << fl_ << "\n";
-        fiBase_ = std::string{};
-        fiData_ = std::vector<char>{};
-        fiOffset_ = 0;
-        fi_ = std::filesystem::path{};
-
+        fi_ = {};
         exit(EXIT_FAILURE);
-      } else if (fiBase_ == fsBase_) {
+      } else if (fi_.base == fs_.base) {
         std::cerr << "Removing FI Data: " << fi_ << "\n";
-        flBase_ = std::string{};
-        flData_ = std::basic_string<char>{};
-        flOffset_ = 0;
-        fl_ = std::filesystem::path{};
-
+        fl_ = {};
         exit(EXIT_FAILURE);
       }
     } else {
@@ -279,7 +274,7 @@ public:
     const std::filesystem::directory_options options = std::filesystem::directory_options::skip_permission_denied;
     return GetFilesFromIterator(std::filesystem::directory_iterator(path, options));// todo may need sorted.
   }
-  template<class iter_t> [[nodiscard]] static FIFLFSmap GetFilesFromIterator(iter_t iter)
+  template<class iter_t>[[nodiscard]] static FIFLFSmap GetFilesFromIterator(iter_t iter)
   {
     auto tmp = FIFLFSmap();
     auto archive = OpenVIII::Archive::FIFLFS();
@@ -294,9 +289,6 @@ public:
     }
     return tmp;
   }
-  [[nodiscard]] std::string FIBaseName() const { return GetBaseName(FI()); }
-  [[nodiscard]] std::string FLBaseName() const { return GetBaseName(FL()); }
-  [[nodiscard]] std::string FSBaseName() const { return GetBaseName(FS()); }
   [[nodiscard]] std::string static GetBaseName(const std::filesystem::path &path)
   {
     if (path.string().empty()) return std::string{};
@@ -307,7 +299,7 @@ public:
   [[nodiscard]] std::string GetBaseName() const
   {
     std::string name;
-    for (const auto &path : { FI(), FL(), FS() }) {
+    for (const auto &path : { fi_.path, fl_.path, fs_.path }) {
       if (!path.string().empty()) {
         name = path.filename().stem().string();
         std::transform(name.begin(), name.end(), name.begin(), ::toupper);
--- Tests/TestExtractSingleFIFLFS.cpp
+++ Tests/TestExtractSingleFIFLFS.cpp
@@ -5,12 +5,23 @@
 #include "TestExtractSingleFIFLFS.h"
 int main()
 {
-  //test FIFLFS
+  // test FIFLFS
   std::string path = R"(C:\Program Files (x86)\Steam\steamapps\common\FINAL FANTASY VIII\Data\lang-en)";
   OpenVIII::Tools::replaceSlashes(path);
-
-  if (!std::filesystem::exists(path)) { return 0; }
+  if (!std::filesystem::exists(path)) { return 1; }
+  std::string needle{};
+  std::cout << "Enter string to search for: ";
+  std::getline(std::cin, needle);
+  OpenVIII::Tools::replaceSlashes(needle);
+  if (needle.empty()) { return 1; }
   const auto files = OpenVIII::Archive::FIFLFS::GetFilesFromPath(path);
-  std::for_each(std::begin(files), std::end(files), &OpenVIII::Archive::FIFLFS::testPair);
+  for (const auto& entry : files) {
+    const auto & fiflfs = entry.second;
+    std::cout <<"Searching "<< fiflfs << '\n';
+    const auto& fl = fiflfs.FL();
+    const auto& [id, innerPath] = OpenVIII::Archive::FL::GetEntry(fl.path,needle,fl.offset);
+    if(!innerPath.empty()) { std::cout << innerPath << '\n'; }
+  }
+  // std::for_each(std::begin(files), std::end(files), &OpenVIII::Archive::FIFLFS::testPair);
   return 0;
 }
