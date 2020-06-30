90509839455638f3a0e15a84c5d523ea2e3164d2
Sebanisu <peeceseven@gmail.com>
2020-06-30 19:04:00 -0400

    Okay everything is working again
    one big project again.

---
+++ src/OpenVIII/Paths/Paths.cpp
@@ -0,0 +1,24 @@
+// This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#include "Paths.h"
+std::array<std::string, OpenVIII::Paths::count> OpenVIII::Paths::get()
+{
+  return { R"(/mnt/c/Program Files (x86)/Steam/steamapps/common/FINAL FANTASY VIII/Data/lang-en)",
+    R"(C:\Program Files (x86)\Steam\steamapps\common\FINAL FANTASY VIII\Data\lang-en)",
+    R"(/mnt/c/Program Files (x86)/Steam/steamapps/common/FINAL FANTASY VIII)",
+    R"(C:\Program Files (x86)\Steam\steamapps\common\FINAL FANTASY VIII)",
+    R"(/mnt/k/ff82000)",
+    R"(K:\ff82000)",
+    R"(C:\Program Files (x86)\Steam\steamapps\common\FINAL FANTASY VIII Remastered)",
+    R"(/mnt/c/Program Files (x86)/Steam/steamapps/common/FINAL FANTASY VIII Remastered)" };
+}
---
+++ src/OpenVIII/Paths/Paths.h
@@ -0,0 +1,24 @@
+// This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#ifndef VIIIARCHIVE_TESTPATHS_H
+#define VIIIARCHIVE_TESTPATHS_H
+#include <array>
+#include <string>
+namespace OpenVIII {
+struct Paths
+{
+  constexpr static auto count = 8;
+  static std::array<std::string, count> get();
+};
+}
+#endif// VIIIARCHIVE_TESTPATHS_H
