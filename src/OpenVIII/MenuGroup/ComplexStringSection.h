39c997092956f09925765e0b0554eb2a1f06cc4b
Sebanisu <peeceseven@gmail.com>
2020-08-17 14:24:33 -0400

    adding complex string section support.

---
+++ src/OpenVIII/MenuGroup/ComplexStringSection.h
@@ -0,0 +1,80 @@
+//
+// Created by pcvii on 8/17/2020.
+//
+
+#ifndef VIIIARCHIVE_COMPLEXSTRINGSECTION_H
+#define VIIIARCHIVE_COMPLEXSTRINGSECTION_H
+#include <cstdint>
+#include <cstring>
+#include <array>
+#include <ostream>
+#include <string_view>
+namespace OpenVIII::MenuGroup {
+struct ComplexStringSectionEntry
+{
+private:
+  static constexpr auto unkCount = 6U;
+  std::array<uint8_t,unkCount> unknown_{};
+  std::string_view buffer_{};
+
+  //http://wiki.ffrtt.ru/index.php?title=FF8/Menu_mngrp_complex_strings#String_Entry
+public:
+  explicit ComplexStringSectionEntry(const std::string_view & buffer)
+  {
+    uint16_t length_{};
+    std::memcpy(unknown_.data(),buffer.data(),sizeof(unknown_));
+    std::memcpy(length_.data(),buffer.data()+sizeof(unknown_),sizeof(length_));
+    buffer_ = std::string_view {buffer.data()+sizeof(unknown_)+sizeof(length_), length_};
+  }
+};
+struct ComplexStringSectionOffsets
+{
+  //http://wiki.ffrtt.ru/index.php?title=FF8/Menu_mngrp_complex_strings#Seek_Struct
+private:
+  std::uint16_t offset_{};
+  std::uint16_t number_{};
+public:
+  [[nodiscard]] auto offset() const noexcept {return offset_;}
+  [[nodiscard]] auto number() const noexcept {return number_;}
+  friend std::ostream & operator << (std::ostream &os, const ComplexStringSectionOffsets & item )
+  {
+    return os << '{' << item.offset() <<", "<< item.number() '}'<<
+  }
+};
+struct ComplexStringSection
+{
+  //http://wiki.ffrtt.ru/index.php?title=FF8/Menu_mngrp_complex_strings
+private:
+  std::uint32_t count_{};
+  std::string_view buffer_{};
+
+public:
+  explicit ComplexStringSection(const std::string_view & buffer)
+  {
+    if(std::size(buffer)>sizeof(count_))
+    {
+      std::memcpy(&count_,buffer.data(),sizeof(count_));
+      buffer_ = std::string_view {buffer.data()+sizeof(count_), std::size(buffer) - sizeof(count_)};
+    }
+  }
+  [[nodiscard]] auto size() const
+  {return count_;}
+  [[nodiscard]] ComplexStringSectionOffsets at(const size_t & id) const
+  {
+    if(id>=count_ || sizeof(d)*id >= std::size(buffer_))
+    {
+      return {}; //throw?
+    }
+    ComplexStringSectionOffsets d{};
+    memcpy(&d, buffer_.data()+(sizeof(d)*id),sizeof(d));
+    return d;
+  }
+
+  [[nodiscard]] ComplexStringSectionEntry at(const ComplexStringSectionOffsets & offsets) const
+  {
+    return {std::string_view {buffer_.data()+offsets.offset(),buffer_.size()+offsets.offset()}}
+  }
+
+};
+}
+#endif// VIIIARCHIVE_COMPLEXSTRINGSECTION_H
