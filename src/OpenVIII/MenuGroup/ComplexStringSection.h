7dcb025c949124c77d180f239fa78f7bcd2d1bde
Sebanisu <peeceseven@gmail.com>
2020-09-01 21:08:36 -0400

    Update ComplexStringSection.h
    updated naming.

--- src/OpenVIII/MenuGroup/ComplexStringSection.h
+++ src/OpenVIII/MenuGroup/ComplexStringSection.h
@@ -14,34 +14,34 @@ namespace open_viii::menu_group {
 struct ComplexStringSectionEntry
 {
 private:
-  static constexpr auto unkCount = 6U;
-  std::array<uint8_t, unkCount> unknown_{};
-  std::string_view buffer_{};
+  static constexpr auto UNK_COUNT = 6U;
+  std::array<uint8_t, UNK_COUNT> m_unknown{};
+  std::string_view m_buffer{};
 
   // http://wiki.ffrtt.ru/index.php?title=FF8/Menu_mngrp_complex_strings#String_Entry
 public:
   explicit ComplexStringSectionEntry(const std::string_view &buffer)
   {
-    uint16_t length_{};
-    std::memcpy(unknown_.data(), buffer.data(), sizeof(unknown_));
-    std::memcpy(&length_, buffer.data() + sizeof(unknown_), sizeof(length_));
-    buffer_ = std::string_view{ buffer.data() + sizeof(unknown_) + sizeof(length_), length_ };
+    uint16_t length{};
+    std::memcpy(m_unknown.data(), buffer.data(), sizeof(m_unknown));
+    std::memcpy(&length, buffer.data() + sizeof(m_unknown), sizeof(length));
+    m_buffer = std::string_view{ buffer.data() + sizeof(m_unknown) + sizeof(length), length };
   }
   template<LangT langVal> std::ostream &out(std::ostream &os) const
   {
-    return os << Tools::u8tosv(FF8String<langVal>::decode(buffer_));
+    return os << Tools::u8tosv(FF8String<langVal>::decode(m_buffer));
   }
 };
 struct ComplexStringSectionOffsets
 {
   // http://wiki.ffrtt.ru/index.php?title=FF8/Menu_mngrp_complex_strings#Seek_Struct
 private:
-  std::uint16_t offset_{};
-  std::uint16_t index_{};
+  std::uint16_t m_offset{};
+  std::uint16_t m_index{};
 
 public:
-  [[nodiscard]] auto offset() const noexcept { return offset_; }
-  [[nodiscard]] auto index() const noexcept { return index_; }
+  [[nodiscard]] auto offset() const noexcept { return m_offset; }
+  [[nodiscard]] auto index() const noexcept { return m_index; }
   friend std::ostream &operator<<(std::ostream &os, const ComplexStringSectionOffsets &item)
   {
     return os << '{' << item.index() << ", " << item.offset() << '}';
@@ -51,34 +51,34 @@ struct ComplexStringSection
 {
   // http://wiki.ffrtt.ru/index.php?title=FF8/Menu_mngrp_complex_strings
 private:
-  std::uint32_t count_{};
-  std::string_view buffer_{};
-  static constexpr auto sectionCount_ = 6U;
-  std::array<std::string_view, sectionCount_> data_{};
+  std::uint32_t m_count{};
+  std::string_view m_buffer{};
+  static constexpr auto SECTION_COUNT = 6U;
+  std::array<std::string_view, SECTION_COUNT> m_data{};
 
 public:
-  ComplexStringSection(const std::string_view &buffer, const std::array<std::string_view, sectionCount_> &data)
-    : data_{ data }
+  ComplexStringSection(const std::string_view &buffer, const std::array<std::string_view, SECTION_COUNT> &data)
+    : m_data{ data }
   {
-    if (std::size(buffer) > sizeof(count_)) {
-      std::memcpy(&count_, buffer.data(), sizeof(count_));
-      buffer_ = std::string_view{ buffer.data() + sizeof(count_), std::size(buffer) - sizeof(count_) };
+    if (std::size(buffer) > sizeof(m_count)) {
+      std::memcpy(&m_count, buffer.data(), sizeof(m_count));
+      m_buffer = std::string_view{ buffer.data() + sizeof(m_count), std::size(buffer) - sizeof(m_count) };
     }
   }
-  [[nodiscard]] auto size() const { return count_; }
+  [[nodiscard]] auto size() const { return m_count; }
   [[nodiscard]] ComplexStringSectionOffsets at(const size_t &id) const
   {
     ComplexStringSectionOffsets d{};
-    if (id >= count_ || sizeof(d) * id >= std::size(buffer_)) {
+    if (id >= m_count || sizeof(d) * id >= std::size(m_buffer)) {
       return {};// throw?
     }
-    memcpy(&d, buffer_.data() + (sizeof(d) * id), sizeof(d));
+    memcpy(&d, m_buffer.data() + (sizeof(d) * id), sizeof(d));
     return d;
   }
 
   [[nodiscard]] auto at(const ComplexStringSectionOffsets &offsets) const
   {
-    const auto temp = data_.at(offsets.index());
+    const auto temp = m_data.at(offsets.index());
     return ComplexStringSectionEntry{ std::string_view{
       temp.data() + offsets.offset(), temp.size() - offsets.offset() } };
   }
