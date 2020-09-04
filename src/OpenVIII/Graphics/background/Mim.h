8f6b1ee5d04708ab6d749162d596a01d12af6b53
Sebanisu <peeceseven@gmail.com>
2020-09-04 18:19:23 -0400

    Update Mim.h
    made the the save function into get-color with lambda to use with the colors temperary data while it's in scope.

--- src/OpenVIII/Graphics/background/Mim.h
+++ src/OpenVIII/Graphics/background/Mim.h
@@ -17,6 +17,15 @@ private:
   std::vector<char> m_buffer{};
   MimType m_mim_type{};
 
+  [[nodiscard]] static const auto &clut_width() noexcept
+  {
+    return open_viii::graphics::background::MimType::COLORS_PER_PALETTE;
+  }
+  [[nodiscard]] std::size_t clut_height() const noexcept
+  {
+    return ((m_mim_type.palette_section_size() - m_mim_type.bytes_skipped_palettes()) / sizeof(Color16)) / clut_width();
+  }
+
 public:
   static constexpr std::array<MimType, 2> TEXTURE_TYPES{ MimType(24, 13), MimType(16, 12, 0, 2) };
   Mim() = default;
@@ -33,43 +42,15 @@ public:
     }
     return {};
   }
-  std::vector<Color16> bpp8(const std::uint8_t &clut_row)
-  {
-
-    const auto buffer = std::span<const char>(m_buffer);
-    const auto image_buffer = buffer.subspan(m_mim_type.palette_section_size());
-    const auto palette_buffer_tmp = buffer.subspan(
-      m_mim_type.bytes_skipped_palettes(), m_mim_type.palette_section_size() - m_mim_type.bytes_skipped_palettes());
-
-    const auto image_buffer_bbp8 =
-      std::span<const std::uint8_t>(reinterpret_cast<const std::uint8_t *>(std::ranges::data(image_buffer)),
-        std::ranges::size(image_buffer) / sizeof(std::uint8_t));
-    const auto palette_buffer_bbp16 =
-      std::span(reinterpret_cast<const Color16 *>(std::ranges::data(palette_buffer_tmp)),
-        std::ranges::size(palette_buffer_tmp) / sizeof(Color16));
-    const uint16_t clut_width = open_viii::graphics::background::MimType::COLORS_PER_PALETTE;
-    const std::size_t clut_height = std::ranges::size(palette_buffer_bbp16) / clut_width;
-    if (clut_row > clut_height) {
-      return {};
-    }
-    std::vector<Color16> out{};
-    out.resize(std::ranges::size(image_buffer_bbp8));
-    const auto palette_row = palette_buffer_bbp16.subspan(clut_row * clut_width, clut_width);
-    std::transform(image_buffer_bbp8.begin(),
-      image_buffer_bbp8.end(),
-      out.begin(),
-      [&palette_row, &clut_row, &clut_width](const std::uint8_t &key) -> Color16 {
-        if (key < std::ranges::size(palette_row)) {
-          return palette_row[key];
-        } else {
-          return {};
-        }
-      });
-    return out;
-  }
   friend std::ostream &operator<<(std::ostream &os, const Mim &m) { return os << m.m_mim_type; }
-  [[maybe_unused]] void save([[maybe_unused]] std::string_view filename) const
-  {
+  template<typename lambdaT>
+  requires(std::invocable<lambdaT, std::span<const Color16>, std::size_t, std::size_t, std::string>)
+    [[maybe_unused]] void get_colors(const std::string_view &filename,
+      const BPPT &bpp,
+      const uint8_t &palette,
+      const lambdaT lambda,
+      bool dump_palette = false) const
+  {// going to get all the colors for image and run the supplied lambda passing the colors.
     const auto buffer = std::span<const char>(m_buffer);
     const auto palette_buffer_tmp = buffer.subspan(
       m_mim_type.bytes_skipped_palettes(), m_mim_type.palette_section_size() - m_mim_type.bytes_skipped_palettes());
@@ -87,60 +68,90 @@ public:
       std::span<const Bit4Values>(reinterpret_cast<const Bit4Values *>(std::ranges::data(image_buffer)),
         std::ranges::size(image_buffer) / sizeof(Bit4Values));
     const auto path = std::filesystem::path(filename);
-    const auto s = ((path.parent_path() / path.stem()).string()) + "_Clut.mim";
-    const uint16_t clut_width = open_viii::graphics::background::MimType::COLORS_PER_PALETTE;
-    const std::size_t clut_height = std::ranges::size(palette_buffer_bbp16) / clut_width;
-    Ppm::save(palette_buffer_bbp16, clut_width, clut_height, s);
-    {
 
-      for (std::size_t i{}; i < clut_height; i++) {
-        std::vector<Color16> out{};
-        out.resize(std::ranges::size(image_buffer_bbp8));
-        std::transform(image_buffer_bbp8.begin(),
-          image_buffer_bbp8.end(),
-          out.begin(),
-          [&i, &palette_buffer_bbp16](std::size_t key) -> Color16 {
-            key += i * clut_width;
-            if (key < std::ranges::size(palette_buffer_bbp16)) {
-              return palette_buffer_bbp16[key];
-            } else {
-              return {};
-            }
-          });
-        const auto s2 = ((path.parent_path() / path.stem()).string()) + "_8bpp_" + std::to_string(i) + ".mim";
-        Ppm::save(out, m_mim_type.width(), std::ranges::size(out) / m_mim_type.width(), s2);
+    if (dump_palette) {
+      lambda(palette_buffer_bbp16,
+        static_cast<std::size_t>(clut_width()),
+        clut_height(),
+        ((path.parent_path() / path.stem()).string()) + "_Clut.mim");
+      return;
+    }
+    if (bpp.bpp8()) {
+
+      std::vector<Color16> out{};
+      out.resize(std::ranges::size(image_buffer_bbp8));
+      std::transform(image_buffer_bbp8.begin(),
+        image_buffer_bbp8.end(),
+        out.begin(),
+        [&palette, &palette_buffer_bbp16](std::size_t key) -> Color16 {
+          key += static_cast<std::size_t>(palette) * static_cast<std::size_t>(clut_width());
+          if (key < std::ranges::size(palette_buffer_bbp16)) {
+            return palette_buffer_bbp16[key];
+          } else {
+            return {};
+          }
+        });
+
+      lambda(out,
+        m_mim_type.width(),
+        std::ranges::size(out) / m_mim_type.width(),
+        ((path.parent_path() / path.stem()).string()) + "_8bpp_" + std::to_string(palette) + ".mim");
+
+    } else if (bpp.bpp4()) {
+      std::vector<Color16> out{};
+      out.reserve(std::ranges::size(image_buffer_bbp4) * 2);
+      std::for_each(image_buffer_bbp4.begin(),
+        image_buffer_bbp4.end(),
+        [&palette, &palette_buffer_bbp16, &out](const Bit4Values &key) {
+          auto k1 = static_cast<std::size_t>(key.first + (palette * clut_width()));
+          auto k2 = static_cast<std::size_t>(key.second + (palette * clut_width()));
+          if (k1 < std::ranges::size(palette_buffer_bbp16)) {
+            out.emplace_back(palette_buffer_bbp16[k1]);
+          } else {
+            out.emplace_back();
+          }
+          if (k2 < std::ranges::size(palette_buffer_bbp16)) {
+            out.emplace_back(palette_buffer_bbp16[k1]);
+          } else {
+            out.emplace_back();
+          }
+        });
+      lambda(out,
+        m_mim_type.width() * 2,
+        std::ranges::size(out) / (m_mim_type.width() * 2),
+        ((path.parent_path() / path.stem()).string()) + "_4bpp_" + std::to_string(palette) + ".mim");
+
+    } else if (bpp.bpp16()) {
+      lambda(image_buffer_bbp16,
+        (m_mim_type.width() / 2U),
+        std::ranges::size(image_buffer_bbp16) / (m_mim_type.width() / 2U),
+        ((path.parent_path() / path.stem()).string()) + "_16bpp.mim");
+    }
+  }
+  [[maybe_unused]] void save([[maybe_unused]] std::string_view filename) const
+  {
+    static constexpr auto ppm_save = [](const std::span<const Color16> &data,
+                                       const std::size_t &width,
+                                       const std::size_t &height,
+                                       const std::string &local_filename) {
+      Ppm::save(data, width, height, local_filename);
+    };
+    get_colors(filename, {}, {}, ppm_save, true);
+    BPPT bpp{};
+    {
+      bpp.bpp4(true);
+      for (std::uint8_t i{}; i < static_cast<std::uint8_t>(clut_height()); i++) {
+        get_colors(filename, bpp, i, ppm_save, false);
       }
     }
     {
-      for (std::size_t i{}; i < clut_height; i++) {
-        std::vector<Color16> out{};
-        out.reserve(std::ranges::size(image_buffer_bbp4) * 2);
-        std::for_each(
-          image_buffer_bbp4.begin(), image_buffer_bbp4.end(), [&i, &palette_buffer_bbp16, &out](const Bit4Values &key) {
-            auto k1 = key.first + (i * clut_width);
-            auto k2 = key.second + (i * clut_width);
-            if (k1 < std::ranges::size(palette_buffer_bbp16)) {
-              out.emplace_back(palette_buffer_bbp16[k1]);
-            } else {
-              out.emplace_back();
-            }
-            if (k2 < std::ranges::size(palette_buffer_bbp16)) {
-              out.emplace_back(palette_buffer_bbp16[k1]);
-            } else {
-              out.emplace_back();
-            }
-          });
-
-
-        const auto s2 = ((path.parent_path() / path.stem()).string()) + "_4bpp_" + std::to_string(i) + ".mim";
-        Ppm::save(out, m_mim_type.width() * 2, std::ranges::size(out) / (m_mim_type.width() * 2), s2);
+      bpp.bpp8(true);
+      for (std::uint8_t i{}; i < static_cast<std::uint8_t>(clut_height()); i++) {
+        get_colors(filename, bpp, i, ppm_save, false);
       }
     }
-    {
-      const auto s2 = ((path.parent_path() / path.stem()).string()) + "_16bpp.mim";
-      std::size_t width_local{ m_mim_type.width() / 2U };
-      Ppm::save(image_buffer_bbp16, width_local, std::ranges::size(image_buffer_bbp16) / width_local, s2);
-    }
+    bpp.bpp16(true);
+    get_colors(filename, bpp, {}, ppm_save, false);
   }
 };
 }// namespace open_viii::graphics::background
