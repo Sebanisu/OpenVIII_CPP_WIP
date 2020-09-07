e8ad6aad552836af490d13d69725e9ce8046cc63
Sebanisu <peeceseven@gmail.com>
2020-09-07 00:35:22 -0400

    Update ppm.h
    I started to try to add ability to read ppm. though i mostly just want to read the files i write.

--- src/OpenVIII/Graphics/ppm.h
+++ src/OpenVIII/Graphics/ppm.h
@@ -7,13 +7,73 @@
 #include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <cctype>
 #include <string_view>
 #include "color.h"
 #include "OpenVIII/concepts.h"
 namespace open_viii::graphics {
 struct Ppm
 {
+private:
+  std::vector<Color24<0, 1, 2>> m_colors{};
 
+public:
+  explicit Ppm(const std::string &buffer)
+  {
+    // the support is mostly limited to the same type that is made by the save function.
+    // I can't use span because span_stream does not exist yet it missed cpp20... so this will need to be a string
+    // stream? I know not to use std::regex but i'm feeling like that's better than what I'm doing here. heh.
+
+
+    // sample header ss << "P6\n# THIS IS A COMMENT\n" << width << " " << height << "\n255\n";
+    auto ss = std::stringstream(buffer);
+    std::string line{};
+    std::string type{};
+    std::string comment{};
+    std::string w_h{};
+    std::uint16_t width{};
+    std::uint16_t height{};
+    std::string bytesize{};
+    while (std::getline(ss, line)) {
+      if (line == "P6") {
+        type = line;
+        continue;
+      }
+      if (line[0] == '#') {
+        comment += line;
+        continue;
+      }
+      if (line == "255") {
+        bytesize = line;
+        continue;
+      }
+      if ([&line, &width, &height, &w_h]() -> bool {
+            char space{};
+            auto lss = std::stringstream(line);
+            lss >> width >> space >> height;
+            return space == '_';
+          }()) {
+        w_h = line;
+        continue;
+      }
+      if (!std::ranges::empty(bytesize) && ((height > 0 && width > 0) || !std::ranges::empty(w_h))) {
+        break;
+      }
+    }
+
+    auto bufferspan = (std::span<const char>(buffer)).subspan(static_cast<std::size_t>(ss.tellg()));
+    if (std::ranges::size(bufferspan) > 0) {
+      auto sz = std::ranges::size(bufferspan) / sizeof(Color24<0, 1, 2>);
+      const auto colorspan =
+        std::span<const Color24<0, 1, 2>>{ reinterpret_cast<const Color24<0, 1, 2> *>(std::ranges::data(bufferspan)),
+          sz };
+      m_colors = { std::ranges::cbegin(colorspan), std::ranges::cend(colorspan) };
+    }
+  }
+  [[nodiscard]] const auto & colors()
+  {
+    return m_colors;
+  }
   template<std::ranges::contiguous_range cT>
   static void save(const cT &data, std::size_t width, std::size_t height, const std::string_view &input)
   {// how do i make the concept reject ranges that aren't of Colors? I'm at least checking for Color down below.
