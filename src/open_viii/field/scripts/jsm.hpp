6629b28190c9ea20c32ec407784de6664da66a11
Sebanisu <peeceseven@gmail.com>
2021-01-19 11:26:51 -0500

    correctly loading the entities vectors.
    https://github.com/Sebanisu/OpenVIII_CPP_WIP/projects/1#card-53070876

--- src/open_viii/field/scripts/jsm.hpp
+++ src/open_viii/field/scripts/jsm.hpp
@@ -18,17 +18,40 @@ struct jsm
 {
 private:
   jsm_header m_header{};
-  //  std::vector<jsm_entity> m_door_entities{};
-  //  std::vector<jsm_entity> m_walk_mesh_entities{};
-  //  std::vector<jsm_entity> m_background_entities{};
-  //  std::vector<jsm_entity> m_other_entities{};
+  std::vector<jsm_entity> m_door_entities{};
+  std::vector<jsm_entity> m_walk_mesh_entities{};
+  std::vector<jsm_entity> m_background_entities{};
+  std::vector<jsm_entity> m_other_entities{};
 
 public:
   explicit jsm(std::span<const char> buffer)
-    : m_header(tools::read_val<jsm_header>(
-      buffer))//,
-              // m_door_entities(tools::read_val<std::vector<jsm_entity>>(buffer.subspan(sizeof(m_header)),m_header.count_door_entities()))
-  {}
+    : m_header(tools::read_val<jsm_header>(buffer)),
+      m_door_entities(tools::read_val<std::vector<jsm_entity>>(
+        buffer.subspan(sizeof(m_header)), m_header.count_door_entities())),
+      m_walk_mesh_entities(tools::read_val<std::vector<jsm_entity>>(
+        buffer.subspan(sizeof(m_header)
+                       + sizeof(jsm_entity) * m_header.count_door_entities()),
+        m_header.count_walk_mesh_line_entities())),
+      m_background_entities(tools::read_val<std::vector<jsm_entity>>(
+        buffer.subspan(sizeof(m_header)
+                       + sizeof(jsm_entity)
+                           * (m_header.count_door_entities()
+                              + m_header.count_walk_mesh_line_entities())),
+        m_header.count_background_entities())),
+      m_other_entities(tools::read_val<std::vector<jsm_entity>>(
+        buffer.subspan(sizeof(m_header)
+                       + sizeof(jsm_entity)
+                           * (m_header.count_door_entities()
+                              + m_header.count_walk_mesh_line_entities())
+                       + m_header.count_background_entities()),
+        m_header.count_other_entities()))
+
+  {
+    assert(std::ranges::size(m_door_entities) == m_header.count_door_entities());
+    assert(std::ranges::size(m_walk_mesh_entities) == m_header.count_walk_mesh_line_entities());
+    assert(std::ranges::size(m_background_entities) == m_header.count_background_entities());
+    assert(std::ranges::size(m_other_entities) == m_header.count_other_entities());
+  }
 };
 }// namespace open_viii::field::scripts
 #endif// VIIIARCHIVE_JSM_HPP
