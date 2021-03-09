b8b7c21822749a27c39f455eefaeb589d76615bc
Sebanisu <peeceseven@gmail.com>
2021-03-09 12:52:50 -0500

    adding ${PROJECT_NAME}_ prefix part 1

--- cmake/lz4.cmake
+++ cmake/lz4.cmake
@@ -4,11 +4,13 @@ FetchContent_Declare(
         GIT_REPOSITORY https://github.com/lz4/lz4.git
         GIT_TAG v1.9.2
 )
-FetchContent_MakeAvailable(lz4_lib)
 FetchContent_GetProperties(lz4_lib)
-add_library(External_lz4
-        ${lz4_lib_SOURCE_DIR}/lib/lz4.h
-        ${lz4_lib_SOURCE_DIR}/lib/lz4.c
-        )
-set_target_properties(External_lz4 PROPERTIES LINKER_LANGUAGE CXX)
-target_include_directories(External_lz4 PUBLIC ${lz4_lib_SOURCE_DIR})
+if (NOT lz4_lib_POPULATED)
+    FetchContent_Populate(lz4_lib)
+    add_library(${PROJECT_NAME}_External_lz4
+            ${lz4_lib_SOURCE_DIR}/lib/lz4.h
+            ${lz4_lib_SOURCE_DIR}/lib/lz4.c
+            )
+    set_target_properties(${PROJECT_NAME}_External_lz4 PROPERTIES LINKER_LANGUAGE CXX)
+    target_include_directories(${PROJECT_NAME}_External_lz4 PUBLIC ${lz4_lib_SOURCE_DIR})
+endif ()
