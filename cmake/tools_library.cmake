38a267e76ec5d86df8ec95133b3edf2e0ec6f44d
Sebanisu <peeceseven@gmail.com>
2021-03-08 20:44:01 -0500

    Update tools_library.cmake

--- cmake/tools_library.cmake
+++ cmake/tools_library.cmake
@@ -1,18 +1,18 @@
 include(FetchContent)
 FetchContent_Declare(
-        ToolsLibrary
+        tools_library
         GIT_REPOSITORY https://github.com/Sebanisu/ToolsLibrary.git
         GIT_TAG origin/main
 )
-#FetchContent_MakeAvailable(ToolsLibrary)
-FetchContent_GetProperties(ToolsLibrary)
-if (NOT ToolsLibrary_POPULATED)
-    FetchContent_Populate(ToolsLibrary)
-    if (NOT EXISTS "${ToolsLibrary_SOURCE_DIR}")
-        message(ERROR "${ToolsLibrary_SOURCE_DIR} does not exist!")
+#FetchContent_MakeAvailable(tools_library)
+FetchContent_GetProperties(tools_library)
+if (NOT tools_library_POPULATED)
+    FetchContent_Populate(tools_library)
+    if (NOT EXISTS "${tools_library_SOURCE_DIR}")
+        message(ERROR "${tools_library_SOURCE_DIR} does not exist!")
     endif ()
-    if (NOT EXISTS "${ToolsLibrary_BINARY_DIR}")
-        message(ERROR "${ToolsLibrary_BINARY_DIR} does not exist!")
+    if (NOT EXISTS "${tools_library_BINARY_DIR}")
+        message(ERROR "${tools_library_BINARY_DIR} does not exist!")
     endif ()
-    add_subdirectory(${ToolsLibrary_SOURCE_DIR} ${ToolsLibrary_BINARY_DIR} EXCLUDE_FROM_ALL)
+    add_subdirectory(${tools_library_SOURCE_DIR} ${tools_library_BINARY_DIR} EXCLUDE_FROM_ALL)
 endif ()
