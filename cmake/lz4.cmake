c376556c82f1c72ce346e0704cb6fa0844fdac13
Sebanisu <peeceseven@gmail.com>
2021-03-09 15:45:34 -0500

    Build finished woo
    removed many target_include_libaries and finished adding prefixes to things.

--- cmake/lz4.cmake
+++ cmake/lz4.cmake
@@ -6,11 +6,12 @@ FetchContent_Declare(
 )
 FetchContent_GetProperties(lz4_lib)
 if (NOT lz4_lib_POPULATED)
+    message(NOTICE "--lz4_lib_POPULATED")
     FetchContent_Populate(lz4_lib)
     add_library(${PROJECT_NAME}_External_lz4
             ${lz4_lib_SOURCE_DIR}/lib/lz4.h
             ${lz4_lib_SOURCE_DIR}/lib/lz4.c
             )
     set_target_properties(${PROJECT_NAME}_External_lz4 PROPERTIES LINKER_LANGUAGE CXX)
-    target_include_directories(${PROJECT_NAME}_External_lz4 PUBLIC ${lz4_lib_SOURCE_DIR})
+    target_include_directories(${PROJECT_NAME}_External_lz4 INTERFACE ${lz4_lib_SOURCE_DIR})
 endif ()
