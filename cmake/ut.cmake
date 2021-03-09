ba1b6eb799ad1c9e3f1e6c2e0a43fc2179e72203
Sebanisu <peeceseven@gmail.com>
2021-03-08 20:01:28 -0500

    fix ut

--- cmake/ut.cmake
+++ cmake/ut.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
         ut
         GIT_REPOSITORY https://github.com/boost-ext/ut.git
-        GIT_TAG        v1.1.8
+        GIT_TAG        origin/master
 )
 #FetchContent_MakeAvailable(ut)
 FetchContent_GetProperties(ut)
