f487cb7850a045ddc514678883513c3f93b96041
Sebanisu <peeceseven@gmail.com>
2021-03-12 21:14:32 -0500

    Create FI test app
    It's empty right now but I haven't coded anything yet.

---
+++ ut/FI.cpp
@@ -0,0 +1,11 @@
+#include <boost/ut.hpp>// single header
+// import boost.ut;        // single module (C++20)
+#include "open_viii/archive/FI.hpp"
+int
+  main()
+{
+  using namespace boost::ut::literals;
+  using namespace boost::ut::operators::terse;
+  using namespace boost::ut;
+  [[maybe_unused]] suite tests = [] {};
+}
