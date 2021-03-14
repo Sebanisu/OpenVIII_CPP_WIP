6f2a22b5f30d363f57539c69778e4d1befa1c98f
Sebanisu <peeceseven@gmail.com>
2021-03-14 11:20:05 -0400

    create FL.cpp

---
+++ ut/FL.cpp
@@ -0,0 +1,10 @@
+#include <boost/ut.hpp>// single header
+// import boost.ut;        // single module (C++20)
+int
+  main()
+{
+  using namespace boost::ut::literals;
+  using namespace boost::ut::operators::terse;
+  using namespace boost::ut;
+  [[maybe_unused]] suite tests = [] {};
+}
