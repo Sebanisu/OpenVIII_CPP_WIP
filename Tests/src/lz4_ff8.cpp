562c79a9d29d32f01ec3c241eb86cf0ec30c478d
Sebanisu <peeceseven@gmail.com>
2021-03-22 15:06:54 -0400

    new exe seems to work now.

--- Tests/src/lz4_ff8.cpp
+++ Tests/src/lz4_ff8.cpp
@@ -1,10 +1,10 @@
 #include "open_viii/compression/L4Z.hpp"
+#include "tl/input.hpp"
 #include "tl/read.hpp"
+#include <array>
 #include <fstream>
 #include <iostream>
-#include <array>
 #include <vector>
-#include "tl/input.hpp"
 std::ofstream
   open_file(std::string_view out)
 {
@@ -19,8 +19,12 @@ std::ofstream
 void
   compress(std::string_view in, std::string_view out)
 {
-  std::ofstream os           = open_file(out);
-  const auto    decompressed = tl::read::entire_file(in, std::vector<char>());
+  const auto decompressed = tl::read::entire_file(in, std::vector<char>());
+  if (std::empty(decompressed)) {
+    std::cerr << "file not found or unable to read " << in << '\n';
+    std::exit(1);
+  }
+  std::ofstream os = open_file(out);
   const auto    compressed =
     open_viii::compression::l4z::compress<std::vector<char>>(decompressed);
   std::array<char, 4> tmp{};
@@ -39,35 +43,41 @@ void
              std::string_view   out,
              unsigned long long expected_size)
 {
-  std::ofstream os         = open_file(out);
-  const auto    compressed = tl::read::entire_file(in, std::vector<char>());
-  const auto input = tl::read::input(compressed);
-  const auto comp_size = input.output<std::uint32_t>();
-  if (std::size(compressed) != comp_size+4U)
+  const auto compressed = tl::read::entire_file(in, std::vector<char>());
+  if (std::empty(compressed) || std::size(compressed) <= 12U) {
+    std::cerr << "file not found or unable to read: " << std::size(compressed) << ", " << in << '\n';
+    std::exit(1);
+  }
+  std::ofstream os = open_file(out);
+   const auto input = tl::read::input(compressed);
+   const auto comp_size = input.output<std::uint32_t>();
+   if (std::size(compressed) != comp_size+4U)
   {
-      std::cerr << "Size of file missmatch: per header file size " << std::size(compressed) << " should equal " << comp_size + 4U << " !!\n";
+      std::cerr << "Size of file missmatch: per header file size " <<
+      std::size(compressed) << " should equal " << comp_size + 4U << " !!\n";
       std::exit(1);
   }
-  const auto    decompressed =
+  const auto decompressed =
     open_viii::compression::l4z::decompress<std::vector<char>>(
-      std::span(compressed).subspan(12), expected_size);
+      std::span(compressed).subspan(12U), expected_size);
   os.write(std::data(decompressed), static_cast<long>(std::size(decompressed)));
 }
 int
   main(int argc, [[maybe_unused]] char *argv[])
 {
-  if (argc == 2) {
-    compress(argv[0], argv[1]);
+  if (argc == 3) {
+    compress(argv[1], argv[2]);
     return 0;
   }
-  if (argc == 4 && std::string_view(argv[0]) == std::string_view("-d")) {
-    decompress(argv[1], argv[2], std::stoull(std::string(argv[3])));
+  if (argc == 5 && std::string_view(argv[1]) == std::string_view("-d")) {
+    decompress(argv[2], argv[3], std::stoull(std::string(argv[4])));
     return 0;
   }
   std::cerr << "Use arguments: [-d] \"input\" \"output\" [expected_size]\n";
   std::cerr << "\t-d\t\t--\t required for decompress\n";
   std::cerr << "\tinput\t\t--\t input path\n";
   std::cerr << "\toutput\t\t--\t output path\n";
-  std::cerr << "\texpected_size\t--\t required for decompress, size in bytes after decompressing\n";
+  std::cerr << "\texpected_size\t--\t required for decompress, size in bytes "
+               "after decompressing\n";
   std::exit(1);
 }
