d54b6d597102d5bbc7feb863bd5ba3025a0efd94
Sebanisu <peeceseven@gmail.com>
2021-03-23 19:47:57 -0400

    Update lz4_ff8.cpp
    bah i had put this into the wrong branch.

--- Tests/src/lz4_ff8.cpp
+++ Tests/src/lz4_ff8.cpp
@@ -6,10 +6,10 @@
 #include <iostream>
 #include <vector>
 std::ofstream
-  open_file(std::string_view out)
+open_file(std::string_view out)
 {
   std::ofstream os{};
-  os.open(std::filesystem::path(out));
+  os.open(std::filesystem::path(out), std::ios::out | std::ios::binary);
   if (!os.is_open()) {
     std::cerr << "failed to open " << out << " for writing.";
     std::exit(1);
@@ -17,7 +17,7 @@ std::ofstream
   return os;
 }
 void
-  compress(std::string_view in, std::string_view out)
+compress(std::string_view in, std::string_view out)
 {
   const auto decompressed = tl::read::entire_file(in, std::vector<char>());
   if (std::empty(decompressed)) {
@@ -28,34 +28,42 @@ void
   const auto    compressed =
     open_viii::compression::l4z::compress<std::vector<char>>(decompressed);
   std::array<char, 4> tmp{};
-  std::uint32_t       i = static_cast<uint32_t>(std::size(compressed) + 8U);
+
+  // write int size+8
+  std::uint32_t i = static_cast<std::uint32_t>(std::size(compressed) + 8U);
   std::memcpy(&tmp, &i, 4);
   os.write(std::data(tmp), 4);
-  std::string_view str = "4ZL_";
+  // write string
+  constexpr static std::string_view str = "4ZL_";
   os.write(std::data(str), 4);
-  i = static_cast<uint32_t>(std::size(compressed));
+  // write int size
+  i = static_cast<std::uint32_t>(std::size(compressed));
   std::memcpy(&tmp, &i, 4);
   os.write(std::data(tmp), 4);
+  // write data
   os.write(std::data(compressed), static_cast<long>(std::size(compressed)));
+  os.flush();
+  os.close();
 }
 void
-  decompress(std::string_view   in,
-             std::string_view   out,
-             unsigned long long expected_size)
+decompress(std::string_view   in,
+           std::string_view   out,
+           unsigned long long expected_size)
 {
   const auto compressed = tl::read::entire_file(in, std::vector<char>());
   if (std::empty(compressed) || std::size(compressed) <= 12U) {
-    std::cerr << "file not found or unable to read: " << std::size(compressed) << ", " << in << '\n';
+    std::cerr << "file not found or unable to read: " << std::size(compressed)
+              << ", " << in << '\n';
     std::exit(1);
   }
-  std::ofstream os = open_file(out);
-   const auto input = tl::read::input(compressed);
-   const auto comp_size = input.output<std::uint32_t>();
-   if (std::size(compressed) != comp_size+4U)
-  {
-      std::cerr << "Size of file missmatch: per header file size " <<
-      std::size(compressed) << " should equal " << comp_size + 4U << " !!\n";
-      std::exit(1);
+  std::ofstream os        = open_file(out);
+  const auto    input     = tl::read::input(compressed);
+  const auto    comp_size = input.output<std::uint32_t>();
+  if (std::size(compressed) != comp_size + 4U) {
+    std::cerr << "Size of file missmatch: per header file size "
+              << std::size(compressed) << " should equal " << comp_size + 4U
+              << " !!\n";
+    std::exit(1);
   }
   const auto decompressed =
     open_viii::compression::l4z::decompress<std::vector<char>>(
@@ -63,7 +71,7 @@ void
   os.write(std::data(decompressed), static_cast<long>(std::size(decompressed)));
 }
 int
-  main(int argc, [[maybe_unused]] char *argv[])
+main(int argc, [[maybe_unused]] char *argv[])
 {
   if (argc == 3) {
     compress(argv[1], argv[2]);
