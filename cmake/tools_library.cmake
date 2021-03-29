cca03ed75604e0dc2834030cccdcbe70ed400e79
Sebanisu <peeceseven@gmail.com>
2021-03-29 08:03:23 -0400

    add GIT_REMOTE_UPDATE_STRATEGY
    to prevent cmake from failing due to local changes.

--- cmake/tools_library.cmake
+++ cmake/tools_library.cmake
@@ -3,6 +3,7 @@ FetchContent_Declare(
         tools_library
         GIT_REPOSITORY https://github.com/Sebanisu/ToolsLibrary.git
         GIT_TAG origin/main
+        GIT_REMOTE_UPDATE_STRATEGY CHECKOUT
 )
 #FetchContent_MakeAvailable(tools_library)
 FetchContent_GetProperties(tools_library)
