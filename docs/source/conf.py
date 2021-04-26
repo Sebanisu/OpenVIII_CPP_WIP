774ac532f005c6ca3da6081b39787ff2f8401d26
Sebanisu <peeceseven@gmail.com>
2021-04-26 09:30:48 -0400

    set theme to furo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,5 @@
+# import furo
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -26,7 +28,7 @@ author = 'Sebanisu'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["breathe", "myst_parser"]
+extensions = ["breathe", "myst_parser", "sphinx_tabs.tabs"]
 
 # Breathe Configuration
 breathe_default_project = "OpenVIII_CPP_WIP"
@@ -44,8 +46,38 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
 
+html_theme = 'furo'  # classic
+html_sidebars = {'**': [
+    "sidebar/scroll-start.html",
+    "sidebar/brand.html",
+    "sidebar/search.html",
+    "sidebar/navigation.html",
+    "sidebar/ethical-ads.html",
+    "sidebar/scroll-end.html",
+]
+}
+html_theme_options = {
+    # # Options of read the docs theme
+    # 'logo_only': False,
+    # 'display_version': False,
+    # 'prev_next_buttons_location': 'bottom',
+    # 'style_external_links': True,
+    # # Toc options
+    # 'collapse_navigation': True,
+    # 'sticky_navigation': False,
+    # 'navigation_depth': 3,
+    # 'includehidden': True,  # to show also hidden TOCs in the menu bar
+    # 'titles_only': False,
+    # # 'canonical_url': "https://docs.nitrokey.com/",
+    # # Options of Divio (divio bases on read the docs theme)
+    # 'analytics_id': '',
+    # 'vcs_pageview_mode': '',
+    # 'show_cloud_banner': False,  # False makes the divio box disappear
+    # 'cloud_banner_url': '',
+    # 'cloud_banner_markup': '',
+    # 'segment_id': ''
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
