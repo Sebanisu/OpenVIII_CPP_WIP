c46bb74cf579ddf0e16621836f2ea97c0f8fed56
Sebanisu <peeceseven@gmail.com>
2021-05-17 14:30:43 -0400

    fiflfs_create started

---
+++ applications/src/fiflfs_create.hpp
@@ -0,0 +1,15 @@
+//
+// Created by pcvii on 5/17/2021.
+//
+
+#ifndef VIIIARCHIVE_FIFLFS_CREATE_HPP
+#define VIIIARCHIVE_FIFLFS_CREATE_HPP
+#include "open_viii/archive/FIFLFS.hpp"
+#include <filesystem>
+#include <iostream>
+#include <string_view>
+void fiflfs_create(std::filesystem::path src, std::filesystem::path dst)
+{
+
+}
+#endif// VIIIARCHIVE_FIFLFS_CREATE_HPP
