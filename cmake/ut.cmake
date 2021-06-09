d6f84a36921d8e5c25db811cd36b0658691ff77b
Sebanisu <peeceseven@gmail.com>
2021-06-09 10:32:31 -0400

    add BOOST_UT_DISABLE_MODULE to fix msvc 16.10 add _fetch prefix

--- cmake/ut.cmake
+++ cmake/ut.cmake
@@ -2,14 +2,16 @@ enable_testing()
 include(FetchContent)
 #options for FetchContent are at https://cmake.org/cmake/help/latest/module/ExternalProject.html
 FetchContent_Declare(
-        ut
+        ut_fetch
         GIT_REPOSITORY https://github.com/boost-ext/ut.git
         GIT_TAG origin/master
         GIT_REMOTE_UPDATE_STRATEGY CHECKOUT
 )
 #FetchContent_MakeAvailable(ut)
-FetchContent_GetProperties(ut)
-if (NOT ut_POPULATED)
-    FetchContent_Populate(ut)
-    add_subdirectory(${ut_SOURCE_DIR} ${ut_BINARY_DIR} EXCLUDE_FROM_ALL)
+FetchContent_GetProperties(ut_fetch)
+if (NOT ut_fetch_POPULATED)
+    FetchContent_Populate(ut_fetch)
+    set(BOOST_UT_DISABLE_MODULE TRUE)
+    add_subdirectory(${ut_fetch_SOURCE_DIR} ${ut_fetch_BINARY_DIR} EXCLUDE_FROM_ALL)
+    unset(BOOST_UT_DISABLE_MODULE)
 endif ()
