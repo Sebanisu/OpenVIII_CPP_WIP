0f9d3852ac5a38fd93c0653a95c5403f474def8b
Sebanisu <peeceseven@gmail.com>
2021-07-13 22:01:47 -0400

    moved the concepts to their own file.

--- src/open_viii/kernel/CommonKernel.hpp
+++ src/open_viii/kernel/CommonKernel.hpp
@@ -5,114 +5,14 @@
 #ifndef OPENVIII_CPP_WIP_COMMONKERNEL_HPP
 #define OPENVIII_CPP_WIP_COMMONKERNEL_HPP
 #include <compare>
+#include "concepts.hpp"
 namespace open_viii::kernel {
-template<typename T>
-concept has_name_offset = requires(T t)
-{
-  t.name_offset();
-};
-template<typename T>
-concept has_description_offset = requires(T t)
-{
-  t.description_offset();
-};
-template<typename T>
-concept has_ability_data_id = requires(T t)
-{
-  t.ability_data_id();
-};
-template<typename T>
-concept has_unknown_flags = requires(T t)
-{
-  t.unknown_flags();
-};
-template<typename T>
-concept has_target = requires(T t)
-{
-  t.target();
-};
-template<typename T>
-concept has_unknown = requires(T t)
-{
-  t.unknown();
-};
-template<typename T>
-concept has_magic_id = requires(T t)
-{
-  t.magic_id();
-};
-template<typename T>
-concept has_attack_type = requires(T t)
-{
-  t.attack_type();
-};
-template<typename T>
-concept has_attack_power = requires(T t)
-{
-  t.attack_power();
-};
-template<typename T>
-concept has_unknown0 = requires(T t)
-{
-  t.unknown0();
-};
-template<typename T>
-concept has_unknown1 = requires(T t)
-{
-  t.unknown1();
-};
-template<typename T>
-concept has_attack_flags = requires(T t)
-{
-  t.attack_flags();
-};
-template<typename T>
-concept has_unknown2 = requires(T t)
-{
-  t.unknown2();
-};
-template<typename T>
-concept has_status_attack_enabler = requires(T t)
-{
-  t.status_attack_enabler();
-};
-template<typename T>
-concept has_persistent_statuses = requires(T t)
-{
-  t.persistent_statuses();
-};
-template<typename T>
-concept has_battle_only_statuses = requires(T t)
-{
-  t.battle_only_statuses();
-};
-template<typename T>
-concept has_attack_param = requires(T t)
-{
-  t.attack_param();
-};
-template<typename T>
-concept has_unknown3 = requires(T t)
-{
-  t.unknown3();
-};
-template<typename T>
-concept has_hit_count = requires(T t)
-{
-  t.hit_count();
-};
-template<typename T>
-concept has_element = requires(T t)
-{
-  t.element();
-};
-
 template<typename T>
 struct CommonKernel : T
 {
 public:
   using this_type = CommonKernel<T>;
-  CommonKernel()  = default;
+  constexpr CommonKernel() = default;
   using T::EXPECTED_SIZE;
   constexpr auto
     operator<=>(const this_type &right) const noexcept = default;
