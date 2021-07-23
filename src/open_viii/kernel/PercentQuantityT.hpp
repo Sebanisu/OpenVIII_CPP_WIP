09a4b187f009834033b1db803daf7763be1cd385
Sebanisu <peeceseven@gmail.com>
2021-07-22 21:29:07 -0400

    changed hex to binary form

--- src/open_viii/kernel/PercentQuantityT.hpp
+++ src/open_viii/kernel/PercentQuantityT.hpp
@@ -7,12 +7,12 @@
 namespace open_viii::kernel {
 enum class PercentQuantityT : std::uint8_t
 {
-  none [[maybe_unused]] = 0x00,// 0% = 0x00
-  sixteenth             = 0x01,// 6.25% = 0x01,
-  eighth                = 0x02,// 12.50% = 0x02
-  quarter               = 0x04,// 25% = 0x04
-  half                  = 0x08,// 50% = 0x08
-  full                  = 0x10,// 100% = 0x10
+  none [[maybe_unused]] = 0b0'0000,// 0% = 0x00
+  sixteenth             = 0b0'0001,// 6.25% = 0x01,
+  eighth                = 0b0'0010,// 12.50% = 0x02
+  quarter               = 0b0'0100,// 25% = 0x04
+  half                  = 0b0'1000,// 50% = 0x08
+  full                  = 0b1'0000,// 100% = 0x10
 };
 }// namespace open_viii::kernel
 #endif// VIIIARCHIVE_PERCENTQUANTITYT_HPP
