8d4090f1a2a5dc752f839926b22dc06aedb3e60f
Sebanisu <peeceseven@gmail.com>
2021-07-26 00:00:16 -0400

    Changed RinoaLimitBreakPart2 to use a CommonKernel Class.

--- src/open_viii/kernel/RinoaLimitBreakPart2.hpp
+++ src/open_viii/kernel/RinoaLimitBreakPart2.hpp
@@ -15,6 +15,7 @@
 #include "AttackFlagsT.hpp"
 #include "AttackTypeT.hpp"
 #include "BattleOnlyStatusesT.hpp"
+#include "CommonKernel.hpp"
 #include "ElementT.hpp"
 #include "PersistentStatusesT.hpp"
 #include "TargetT.hpp"
@@ -40,117 +41,46 @@ namespace open_viii::kernel {
  * @see
  * https://github.com/DarkShinryu/doomtrain/wiki/Rinoa-limit-breaks-(part-2)
  */
-struct RinoaLimitBreakPart2
+struct RinoaLimitBreakPart2_impl
 {
-private:
-  EncodedStringOffset m_name_offset{};
-  std::uint16_t       m_magic_id{};
-  AttackTypeT         m_attack_type{};
-  std::uint8_t        m_attack_power{};
-  AttackFlagsT        m_attack_flags{};
-  std::uint8_t        m_unknown0{};
-  TargetT             m_target{};
-  std::uint8_t        m_unknown1{};
-  std::uint8_t        m_hit_count{};
-  ElementT            m_element{};
-  std::uint8_t        m_element_attack_percent{};
-  std::uint8_t        m_status_attack_enabler{};
-  PersistentStatusesT m_persistent_statuses{}; // statuses 0-7
-  BattleOnlyStatusesT m_battle_only_statuses{};// statuses 8-39
+protected:
+  EncodedStringOffset m_name_offset            = {};
+  std::uint16_t       m_magic_id               = {};
+  AttackTypeT         m_attack_type            = {};
+  std::uint8_t        m_attack_power           = {};
+  AttackFlagsT        m_attack_flags           = {};
+  std::uint8_t        m_unknown0               = {};
+  TargetT             m_target                 = {};
+  std::uint8_t        m_unknown1               = {};
+  std::uint8_t        m_hit_count              = {};
+  ElementT            m_element                = {};
+  std::uint8_t        m_element_attack_percent = {};
+  std::uint8_t        m_status_attack_enabler  = {};
+  PersistentStatusesT m_persistent_statuses    = {};// statuses 0-7
+  BattleOnlyStatusesT m_battle_only_statuses   = {};// statuses 8-39
+  constexpr RinoaLimitBreakPart2_impl()        = default;
+  static constexpr std::size_t EXPECTED_SIZE   = 20U;
+
 public:
   constexpr auto
-    operator<=>(const RinoaLimitBreakPart2 &right) const noexcept = default;
-  [[nodiscard]] constexpr auto
-    name_offset() const noexcept
-  {
-    return m_name_offset;
-  }
-  [[nodiscard]] constexpr auto
-    magic_id() const noexcept
-  {
-    return m_magic_id;
-  }
-  [[nodiscard]] constexpr auto
-    attack_type() const noexcept
-  {
-    return m_attack_type;
-  }
-  [[maybe_unused]] [[nodiscard]] constexpr auto
-    attack_power() const noexcept
-  {
-    return m_attack_power;
-  }
-  [[nodiscard]] constexpr auto
-    attack_flags() const noexcept
-  {
-    return m_attack_flags;
-  }
-  [[nodiscard]] constexpr auto
-    unknown0() const noexcept
-  {
-    return m_unknown0;
-  }
-  [[nodiscard]] constexpr auto
-    target() const noexcept
-  {
-    return m_target;
-  }
-  [[nodiscard]] constexpr auto
-    unknown1() const noexcept
-  {
-    return m_unknown1;
-  }
-  [[nodiscard]] constexpr auto
-    hit_count() const noexcept
-  {
-    return m_hit_count;
-  }
-  [[nodiscard]] constexpr auto
-    element() const noexcept
-  {
-    return m_element;
-  }
-  [[maybe_unused]] [[nodiscard]] constexpr auto
-    element_attack_percent() const noexcept
-  {
-    return m_element_attack_percent;
-  }
-  [[nodiscard]] constexpr auto
-    status_attack_enabler() const noexcept
-  {
-    return m_status_attack_enabler;
-  }
-  [[nodiscard]] constexpr auto
-    persistent_statuses() const noexcept
-  {
-    return m_persistent_statuses;
-  }// statuses 0-7
-  [[nodiscard]] constexpr auto
-    battle_only_statuses() const noexcept
-  {
-    return m_battle_only_statuses;
-  }// statuses 8-39
-  std::ostream &
-    out(std::ostream &                                os,
-        [[maybe_unused]] const std::span<const char> &buffer) const
-  {
-    return os
-        << ", " << static_cast<std::uint32_t>(m_magic_id) << ", "
-        << static_cast<std::uint32_t>(m_attack_type) << ", "
-        << static_cast<std::uint32_t>(m_attack_power) << ", "
-        << static_cast<std::uint32_t>(m_attack_flags) << ", "
-        << static_cast<std::uint32_t>(m_unknown0) << ", "
-        << static_cast<std::uint32_t>(m_target) << ", "
-        << static_cast<std::uint32_t>(m_unknown1) << ", "
-        << static_cast<std::uint32_t>(m_hit_count) << ", "
-        << static_cast<std::uint32_t>(m_element) << ", "
-        << static_cast<std::uint32_t>(m_element_attack_percent) << ", "
-        << static_cast<std::uint32_t>(m_status_attack_enabler) << ", "
-        << static_cast<std::uint32_t>(m_persistent_statuses)// statuses 0-7
-        << ", "
-        << static_cast<std::uint32_t>(m_battle_only_statuses)// statuses 8-39
-      ;
-  }
+    operator<=>(
+      const RinoaLimitBreakPart2_impl &right) const noexcept = default;
 };
+using RinoaLimitBreakPart2 = CommonKernel<RinoaLimitBreakPart2_impl>;
+static_assert(RinoaLimitBreakPart2::EXPECTED_SIZE == sizeof(RinoaLimitBreakPart2));
+static_assert(has_name_offset<RinoaLimitBreakPart2>);
+static_assert(has_magic_id<RinoaLimitBreakPart2>);
+static_assert(has_attack_type<RinoaLimitBreakPart2>);
+static_assert(has_attack_power<RinoaLimitBreakPart2>);
+static_assert(has_attack_flags<RinoaLimitBreakPart2>);
+static_assert(has_unknown0<RinoaLimitBreakPart2>);
+static_assert(has_target<RinoaLimitBreakPart2>);
+static_assert(has_unknown1<RinoaLimitBreakPart2>);
+static_assert(has_hit_count<RinoaLimitBreakPart2>);
+static_assert(has_element<RinoaLimitBreakPart2>);
+static_assert(has_element_attack_percent<RinoaLimitBreakPart2>);
+static_assert(has_status_attack_enabler<RinoaLimitBreakPart2>);
+static_assert(has_persistent_statuses<RinoaLimitBreakPart2>);// statuses 0-7
+static_assert(has_battle_only_statuses<RinoaLimitBreakPart2>);// statuses 8-39
 }// namespace open_viii::kernel
 #endif// VIIIARCHIVE_RINOALIMITBREAKPART2_HPP
