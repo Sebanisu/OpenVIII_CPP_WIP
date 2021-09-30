d7375c857aadf6c7e284b290b8bacdd002aeed85
Sebanisu <peeceseven@gmail.com>
2021-09-30 18:05:02 -0400

    extract all now tries all of the language codes.
    This will be dumb for cd version as it doesn't track what language it is. and 2013 version won't do anything different unless you have them all installed. Remaster is the only one that has all languages built in.

--- applications/src/TestExtractALL.cpp
+++ applications/src/TestExtractALL.cpp
@@ -17,22 +17,22 @@ int
   main()
 {
   const auto start = std::chrono::steady_clock::now();
-  open_viii::Paths::for_each_path([](const std::filesystem::path &path) {
-    std::cout << path << std::endl;
-    static constexpr auto coo      = open_viii::LangT::en;
-    const auto            archives = open_viii::archive::Archives(
-      path, open_viii::LangCommon::to_string<coo>());
-    if (!static_cast<bool>(archives)) {
-      std::cerr << "Failed to load path: " << path.string() << '\n';
-      return;
-    }
-    [[maybe_unused]] static constexpr auto dump =
-      [](const std::vector<char> &in_buffer, const std::string &in_path) {
-        open_viii::tools::write_buffer(in_buffer, in_path);
-      };
-    archives.execute_on({}, dump);
-    // archives.get<open_viii::archive::ArchiveTypeT::field>().execute_with_nested({},dump,{});
-  });
+  const auto coos  = open_viii::LangCommon::to_string_array();
+  for (const auto &coo : coos)
+    open_viii::Paths::for_each_path([&coo](const std::filesystem::path &path) {
+      std::cout << path << std::endl;
+      const auto archives = open_viii::archive::Archives(path, coo);
+      if (!static_cast<bool>(archives)) {
+        std::cerr << "Failed to load path: " << path.string() << '\n';
+        return;
+      }
+      [[maybe_unused]] static constexpr auto dump
+        = [](const std::vector<char> &in_buffer, const std::string &in_path) {
+            open_viii::tools::write_buffer(in_buffer, in_path);
+          };
+      archives.execute_on({}, dump);
+      // archives.get<open_viii::archive::ArchiveTypeT::field>().execute_with_nested({},dump,{});
+    });
   const auto end  = std::chrono::steady_clock::now();
   const auto diff = end - start;
   std::cout << std::chrono::duration<double, std::milli>(diff).count() << " ms"
