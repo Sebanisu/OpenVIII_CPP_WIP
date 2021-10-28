137e6a28492b3f2d108a8b51b87e56257819ee3f
Sebanisu <peeceseven@gmail.com>
2021-10-27 20:10:29 -0400

    evadence of segfault in gcc.

---
+++ reduce/BackgroundTiles.cpp
@@ -0,0 +1,13 @@
+struct a {
+  bool b();
+};
+template <typename c> struct d : c {
+  c::e;
+  using f = d;
+  constexpr f g(decltype(e.b())) { this; }
+};
+struct h {
+  a e;
+};
+using i = d<h>;
+auto j = i{}.g(1);
