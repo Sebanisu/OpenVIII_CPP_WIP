171ed6106ff89abb49ba5fbe01225fcc40f0fe65
Sebanisu <peeceseven@gmail.com>
2022-12-20 13:10:34 -0500

    update what version of boost::ut we're using. also use boost::ut from vcpkg.
    also update appveyor to disable building docs.
    disable warning that conflicts with boost::ut.

--- cmake/add_test_common.cmake
+++ cmake/add_test_common.cmake
@@ -7,10 +7,11 @@ function(add_test_common file_name)
             )
     target_include_directories(${PROJECT_NAME}_${file_name} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR})
     set_target_properties(${PROJECT_NAME}_${file_name} PROPERTIES OUTPUT_NAME "${file_name}")
+    target_compile_definitions(${PROJECT_NAME}_${file_name} PRIVATE BOOST_UT_DISABLE_MODULE=1)
     add_test(
             NAME ${PROJECT_NAME}_${file_name} COMMAND ${PROJECT_NAME}_${file_name}
     )
-#    target_compile_definitions(${PROJECT_NAME}_${file_name}
-#            PRIVATE BOOST_UT_DISABLE_MODULE
-#            )
+    #    target_compile_definitions(${PROJECT_NAME}_${file_name}
+    #            PRIVATE BOOST_UT_DISABLE_MODULE
+    #            )
 endfunction()
