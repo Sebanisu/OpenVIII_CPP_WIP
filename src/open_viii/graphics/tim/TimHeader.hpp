4024d7403b9e8b8367f9e27890dc5520d01fe5e1
Sebanisu <peeceseven@gmail.com>
2023-03-22 15:40:03 -0400

    Update TimHeader.hpp

--- src/open_viii/graphics/tim/TimHeader.hpp
+++ src/open_viii/graphics/tim/TimHeader.hpp
@@ -23,63 +23,92 @@ struct TimHeader
 {
 private:
   /**
-   * @brief expected value of tag_ is 0x10.
+   * @brief The expected value of TAG_VAL is 0x10.
    */
   static constexpr auto TAG_VAL     = 0x10;
+
   /**
-   * @brief expected value of version_ is 0x0.
+   * @brief The expected value of VERSION_VAL is 0x0.
    */
   static constexpr auto VERSION_VAL = 0x0;
+
   /**
-   * @brief tag_ is first byte of a TIM file.
+   * @brief The first byte of a TIM file, representing the tag.
    */
-  std::uint8_t          m_tag{};
+  std::uint8_t          m_tag       = {};
+
   /**
-   * @brief version_ is second byte of a TIM file.
+   * @brief The second byte of a TIM file, representing the version.
    */
-  std::uint8_t          m_version{};
+  std::uint8_t          m_version   = {};
+
   /**
-   * @brief value should be 0.
+   * @brief Reserved byte, expected value is 0.
    */
-  std::uint8_t          m_none0{};
+  std::uint8_t          m_none0     = {};
+
   /**
-   * @brief value should be 0.
+   * @brief Reserved byte, expected value is 0.
    */
-  std::uint8_t          m_none1{};
+  std::uint8_t          m_none1     = {};
+
   /**
-   * @brief bits per pixel flags and CLP flag
+   * @brief Bits per pixel flags and CLP flag.
    */
-  BPPT                  m_bpp{};
+  BPPT                  m_bpp       = {};
+
   /**
-   * @brief value should be 0.
+   * @brief Reserved byte, expected value is 0.
    */
-  std::uint8_t          m_none2{};
+  std::uint8_t          m_none2     = {};
+
   /**
-   * @brief value should be 0.
+   * @brief Reserved byte, expected value is 0.
    */
-  std::uint8_t          m_none3{};
+  std::uint8_t          m_none3     = {};
+
   /**
-   * @brief value should be 0 or 22
+   * @brief Reserved byte, expected value is either 0 or 22.
    */
-  std::uint8_t          m_none4{};
+  std::uint8_t          m_none4     = {};
 
 public:
-  constexpr TimHeader() = default;
-  constexpr auto
-    operator<=>(const TimHeader &) const = default;
   /**
-   * @return bits per pixel flags and CLP flag
+   * @brief The size of the `TimHeader` struct in bytes.
+   *
+   * This constant stores the expected size of the `TimHeader` struct, in bytes.
+   * It is used in a static assertion to ensure that the size of the `TimHeader`
+   * struct matches the expected size at compile time.
+   */
+  static constexpr auto SIZE = std::size_t{ 8U };
+
+  /**
+   * @brief Default constructor.
+   */
+  constexpr TimHeader()      = default;
+
+  /**
+   * @brief Default three-way comparison.
+   */
+  constexpr auto
+    operator<=>(const TimHeader &) const
+    = default;
+
+  /**
+   * @brief Get the bits per pixel flags and CLP flag.
+   * @return The value of m_bpp.
    */
   [[nodiscard]] constexpr auto
     bpp() const
   {
     return m_bpp;
   }
+
   /**
-   * All none values should be 0. version_ should be versionVal_. tag_ should be
-   * tagVal_. Only valid values for bpp are 4bpp with CLP, 8bpp with CLP, 16bpp,
-   * or 24 bpp.
    * @brief Test member variables for valid values.
+   * All none values should be 0. version_ should be VERSION_VAL.
+   * tag_ should be TAG_VAL. Only valid values for bpp are 4bpp with CLP,
+   * 8bpp with CLP, 16bpp, or 24 bpp.
    * @return true if Check test passes.
    */
   [[nodiscard]] constexpr bool
@@ -89,10 +118,22 @@ public:
         && /*(none4_ == 0 || none4_ == 22) &&*/ m_tag == TAG_VAL
         && m_version == VERSION_VAL && m_bpp.check();
   }
+
+  /**
+   * @brief Implicit conversion to bool, calls check() method.
+   * @return true if Check test passes.
+   */
   [[nodiscard]] explicit constexpr operator bool() const
   {
     return check();
   }
+
+  /**
+   * @brief Overloaded ostream operator for TimHeader.
+   * @param os Output stream.
+   * @param input TimHeader instance to output.
+   * @return Modified output stream.
+   */
   friend std::ostream &
     operator<<(std::ostream &os, const TimHeader &input)
   {
@@ -102,5 +143,9 @@ public:
               << input.m_bpp << '}';
   }
 };
+/**
+ * Ensures that the size of the TimHeader object matches its expected size.
+ */
+static_assert(sizeof(TimHeader) == TimHeader::SIZE);
 }// namespace open_viii::graphics
 #endif// VIIIARCHIVE_TIMHEADER_HPP
