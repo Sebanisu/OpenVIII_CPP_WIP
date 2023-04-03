86b17d8b5da31d6421bf1d64819f2a09ea77de6e
Sebanisu <peeceseven@gmail.com>
2023-04-03 09:38:16 -0400

    Update CharactersT.hpp
    include header

--- src/open_viii/kernel/CharactersT.hpp
+++ src/open_viii/kernel/CharactersT.hpp
@@ -12,6 +12,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #ifndef VIIIARCHIVE_CHARACTERST_HPP
 #define VIIIARCHIVE_CHARACTERST_HPP
+#include <cstdint>
 namespace open_viii::kernel {
 /**
  * Character IDs
