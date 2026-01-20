5b108546c03e836765828683ff841cc317b1f2ff
Sebanisu <peeceseven@gmail.com>
2026-01-20 10:22:22 -0500

    fix format.sh it was failing to process all the files

--- format.sh
+++ format.sh
@@ -1,25 +1,20 @@
 #!/usr/bin/env bash
-# format.sh - format files with clang-format
-
 set -euo pipefail
 
-# Path to clang-format (adjust if needed)
 CLANG_FORMAT="${CLANG_FORMAT:-clang-format}"
 
-# If a file is passed as argument, format that file
 if [[ $# -gt 0 ]]; then
     for file in "$@"; do
         echo "Formatting $file"
         "$CLANG_FORMAT" -i --style=file "$file"
     done
 else
-    # No arguments: format all tracked source files
-    files=$(git ls-files '*.cpp' '*.hpp' '*.h' '*.c' '*.cc' '*.hh')
-    for file in $files; do
-        echo "Formatting $file"
-        "$CLANG_FORMAT" -i --style=file "$file"
-    done
+    # Safe way to iterate over git ls-files
+    git ls-files '*.cpp' '*.hpp' '*.h' '*.c' '*.cc' '*.hh' \
+        | while IFS= read -r file; do
+            echo "Formatting $file"
+            "$CLANG_FORMAT" -i --style=file "$file"
+        done
 fi
 
 echo "Done!"
-
